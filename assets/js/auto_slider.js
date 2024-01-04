690619af595b6439000e2830685e761bf81f5084
abdulla bashir <3bdalla995@gmail.com>
2024-01-04 07:12:27 +0300

    Update auto_slider.js
    added dark-mode switcher code

--- assets/js/auto_slider.js
+++ assets/js/auto_slider.js
@@ -12,3 +12,18 @@ const removeActiveClasses = () => {
     panel.classList.remove("active");
   });
 };
+
+    // Dark-mode Switcher Functionality
+    document.getElementById('toggleContrast').addEventListener('click', function() {
+    document.body.classList.toggle('dark-mode');
+    
+    // Save user's preference to local storage
+    const isDarkMode = document.body.classList.contains('dark-mode');
+    localStorage.setItem('darkMode', isDarkMode);
+});
+
+// Check for user's preference on page load
+const isDarkMode = localStorage.getItem('darkMode');
+if (isDarkMode === 'true') {
+    document.body.classList.add('dark-mode');
+}
